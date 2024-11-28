d670e925f95f6dcb682a9cac37541463a0d4f223
Aleksa Djekanovic <djekatore@Aleksas-Laptop.local>
2024-11-28 09:41:28 +0100

    w

--- electron.js
+++ electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Tray } = require("electron");
+const { app, BrowserWindow, Tray, ipcMain, ipcRenderer } = require("electron");
 const path = require("path");
 const { toggleWindow } = require("./utils");
 
@@ -8,8 +8,8 @@ app.whenReady().then(() => {
   app.dock.hide();
 
   const browserWindow = new BrowserWindow({
-    width: 250,
-    height: 250,
+    width: 200,
+    height: 150,
     show: false,
     frame: false,
     webPreferences: {
@@ -18,12 +18,74 @@ app.whenReady().then(() => {
     },
   });
 
-
-  browserWindow.loadFile('dist/index.html');
+  browserWindow.loadFile("dist/index.html");
 
   tray.addListener("mouse-up", () => {
     toggleWindow(browserWindow, tray);
   });
 
+  ipcMain.on("set-title", (event, message) => {
+    console.log("Received message in main process:", message);
+  });
+
+  runCommand(browserWindow);
+
   tray.setToolTip("Internet speed infos");
 });
+
+const sendSpeedUpdate = (window, value) => {
+  window.webContents.send("speed-update", value);
+};
+
+const runCommand = (window) => {
+  const { spawn } = require("node:child_process");
+
+  var child = spawn("networkQuality");
+  child.stdin.end();
+
+  child.on("error", (err) => {
+    console.log("stderr: <" + err + ">");
+  });
+
+  child.stdout.on("data", (data) => {
+    const response = parseMessage(data.toString());
+    sendSpeedUpdate(window, JSON.stringify(response));
+  });
+
+  child.stderr.on("data", (data) => {
+    console.log("stderr: <" + data + ">");
+  });
+
+  child.on("close", (code) => {
+    if (code === 0) {
+      console.log("Process is finished");
+    }
+  });
+};
+const uplinkLabel = "Uplink capacity:";
+const downlinkLabel = "Downlink capacity:";
+const mbpsLabel = "Mbps";
+
+const parseMessage = (message) => {
+  const uplinkData = message.slice(
+    message.indexOf(uplinkLabel) + uplinkLabel.length,
+    message.indexOf(mbpsLabel) + mbpsLabel.length
+  );
+
+  // cut first parr
+  let messageAfterUplink = message.slice(
+    message.indexOf(mbpsLabel) + mbpsLabel.length
+  );
+
+  const downlinkData = messageAfterUplink.slice(
+    messageAfterUplink.indexOf(downlinkLabel) + downlinkLabel.length,
+    messageAfterUplink.indexOf(mbpsLabel) + mbpsLabel.length
+  );
+
+  console.log("Uplink capacity: ", uplinkData);
+  console.log("Downlink capacity: ", downlinkData);
+  return {
+    uplink: uplinkData,
+    downlink: downlinkData,
+  };
+};
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,101 @@
 <template>
-  <div>Pozdrav</div>
+  <div class="wrapper">
+    <p class="title">Internet Speed</p>
+    <div v-if="isLoading" class="loader"></div>
+    <div v-else class="speed-container">
+      <div class="speed-label">Download: {{ downlink }}</div>
+      <div class="speed-label">Upload: {{ uplink }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
+const { onMounted, ref } = require("vue");
+
 export default {
   name: "App",
+  setup() {
+    const ipcRenderer = window.require("electron").ipcRenderer;
+
+    const uplink = ref(0);
+    const downlink = ref(0);
+
+    const isLoading = ref(true);
+
+    onMounted(() => {
+      // Listen for messages from main process
+      ipcRenderer.on("speed-update", (event, message) => {
+        console.log("Speed update recieved:", message);
+
+        const response = JSON.parse(message.toString());
+
+        uplink.value = response.uplink;
+        downlink.value = response.downlink;
+
+        isLoading.value = false;
+      });
+    });
+    return {
+      uplink,
+      downlink,
+      isLoading,
+    };
+  },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+
+  background-color: #558888;
+  overflow: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  height: 250px;
-  width: 250px;
+  color: #fff;
+  font-size: 16px;
+
+  width: 100%;
+  height: 100%;
+
+  border-radius: 10px;
+}
+
+.wrapper {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 0;
+  flex-direction: column;
+
+  .title {
+    font-size: 20px;
+  }
+}
+
+.loader {
+  border: 5px solid #f3f3f3; /* Light grey */
+  border-top: 5px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
